503d7d94f12479c2839f125883b1a7141e739d53
Hoerin Doh <hellodhlyn@gmail.com>
2024-11-08 01:20:52 +0900

    fix: add missing parameter

--- app/graphql/queries/raids_query.rb
+++ app/graphql/queries/raids_query.rb
@@ -5,7 +5,7 @@ module Queries
     argument :until_after, GraphQL::Types::ISO8601DateTime, required: false
     argument :raid_ids, [String], required: false
 
-    def resolve(until_after: nil)
+    def resolve(until_after: nil, raid_ids: nil)
       results = Raid.order(since: :asc, until: :asc)
       results = results.where("until >= ?", until_after) if until_after.present?
       results = results.where(raid_id: raid_ids) if raid_ids.present?
