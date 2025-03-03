504a7ffa5dce18dcf8efd25de7e95f0d85cb352b
Hoerin Doh <hellodhlyn@gmail.com>
2025-03-04 00:41:38 +0900

    feat: additional filters for events and raids queries

--- app/graphql/queries/events_query.rb
+++ app/graphql/queries/events_query.rb
@@ -2,12 +2,14 @@ module Queries
   class EventsQuery < Queries::BaseQuery
     type Types::EventType.connection_type, null: false
 
-    argument :until_after, GraphQL::Types::ISO8601DateTime, required: false
+    argument :until_after,  GraphQL::Types::ISO8601DateTime, required: false
+    argument :since_before, GraphQL::Types::ISO8601DateTime, required: false
     argument :event_ids, [String], required: false
 
-    def resolve(until_after: nil, event_ids: nil)
+    def resolve(until_after: nil, since_before: nil, event_ids: nil)
       results = Event.order(since: :asc, until: :asc)
       results = results.where("until >= ?", until_after) if until_after.present?
+      results = results.where("since < ?", since_before) if since_before.present?
       results = results.where(event_id: event_ids) if event_ids.present?
       results
     end
--- app/graphql/queries/raids_query.rb
+++ app/graphql/queries/raids_query.rb
@@ -2,13 +2,17 @@ module Queries
   class RaidsQuery < Queries::BaseQuery
     type Types::RaidType.connection_type, null: false
 
-    argument :until_after, GraphQL::Types::ISO8601DateTime, required: false
+    argument :until_after,  GraphQL::Types::ISO8601DateTime, required: false
+    argument :since_before, GraphQL::Types::ISO8601DateTime, required: false
     argument :raid_ids, [String], required: false
+    argument :types, [Types::RaidType::RaidTypeEnum], required: false
 
-    def resolve(until_after: nil, raid_ids: nil)
+    def resolve(until_after: nil, since_before: nil, raid_ids: nil, types: nil)
       results = Raid.order(since: :asc, until: :asc)
       results = results.where("until >= ?", until_after) if until_after.present?
+      results = results.where("since < ?", since_before) if since_before.present?
       results = results.where(raid_id: raid_ids) if raid_ids.present?
+      results = results.where(type: types) if types.present?
       results
     end
   end
