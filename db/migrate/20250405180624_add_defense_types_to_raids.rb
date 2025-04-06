dc5239dc491e33994a41e22adc55d96c6c6ca87f
Hoerin Doh <hellodhlyn@gmail.com>
2025-04-06 14:19:55 +0900

    fix: invalid access to attribute during schema migration

--- db/migrate/20250405180624_add_defense_types_to_raids.rb
+++ db/migrate/20250405180624_add_defense_types_to_raids.rb
@@ -2,7 +2,7 @@ class AddDefenseTypesToRaids < ActiveRecord::Migration[8.0]
   def change
     add_column :raids, :defense_types, :jsonb, default: []
     Raid.all.each do |raid|
-      raid.update!(defense_types: [{ defense_type: raid.defense_type, difficulty: nil }])
+      raid.update!(defense_types: [{ defense_type: raid.read_attribute(:defense_type), difficulty: nil }])
     end
   end
 end
